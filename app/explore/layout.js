9c9ceb5dc6af8a3fe5a89f59ea97d2d946cd1e2a
nick <nick.m.dodson@gmail.com>
2023-12-12 12:12:44 -0500

    first

---
+++ app/explore/layout.js
@@ -0,0 +1,9 @@
+
+export default function Layout({ children }) {
+  return (
+    <div className="w-[90vw] m-auto">
+      {children}
+    </div>
+  );
+}
+
---
+++ app/explore/page.js
@@ -0,0 +1,35 @@
+export const revalidate = 60;
+
+
+const getSpaces = async () => {
+  const data = await fetch(`http://localhost:8080/api/graphql`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+      query Spaces{
+        spaces{
+            name
+            displayName
+            members
+            admins{
+                address
+            }
+            logoUrl
+        }
+    }`,
+    }),
+    next: { tags: ["spaces"] },
+    cache: 'force-cache'
+  }).then((res) => res.json());
+  return data;
+};
+
+
+export default async function Page(){
+	const spaces = await getSpaces();
+  	return <pre>{JSON.stringify(spaces, null, 2)}</pre>
+
+}
